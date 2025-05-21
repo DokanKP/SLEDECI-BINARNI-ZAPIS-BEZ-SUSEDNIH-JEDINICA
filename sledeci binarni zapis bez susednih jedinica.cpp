e86c59f4a5bf354385ba78d8088e6a4e75ffdb24
DokanKP <141933267+DokanKP@users.noreply.github.com>
2025-05-21 02:50:08 +0200

    Update sledeci binarni zapis bez susednih jedinica.cpp

--- sledeci binarni zapis bez susednih jedinica.cpp
+++ sledeci binarni zapis bez susednih jedinica.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+// provera ako postoje susedne jedinice u nizu
 bool provera(string &n){
     for (int i = 1; i < n.size(); i++){
         if (n[i] == '1' && n[i-1] == '1'){
@@ -15,9 +16,9 @@ int main()
 {
     string n;
     cin >> n;
-    string res = n;
+    string res = n; // isprobavamo moguce resenje
     for (int i = n.size()-1; i >= 0; i--){
-        if (n[i] == '0'){
+        if (n[i] == '0'){ // trazimo najmanji sledeci niz
             res[i] = '1';
             for (int j = i + 1; j < n.size(); j++){
                 res[j] = '0';
